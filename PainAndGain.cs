09e2ca949eebe9f8ed2a1ef2e9a85b461317a056
Yeeto Deleto <44147364+Cataclyzmicc@users.noreply.github.com>
2018-10-19 10:20:15 -0400

    Create PainAndGain.cs

---
+++ PainAndGain.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Graphics.Effects;
+using Terraria.Graphics.Shaders;
+using Terraria.ID;
+using Terraria.ModLoader;
+using PainAndGain.Items;
+
+namespace PainAndGain
+{
+    public class PainAndGain : Mod
+    {
+        public PainAndGain()
+        {
+            Properties = new ModProperties()
+            {
+                Autoload = true,
+                AutoloadGores = true,
+                AutoloadSounds = true
+            };
+        }
+    }
+}
