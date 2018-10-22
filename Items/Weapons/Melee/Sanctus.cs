669aa3d62d8bea3cc9dfbda5026316ff1270fa76
Yeeto Deleto <44147364+Cataclyzmicc@users.noreply.github.com>
2018-10-22 18:13:26 -0400

    yeet

--- Items/Weapons/Melee/Sanctus.cs
+++ Items/Weapons/Melee/Sanctus.cs
@@ -10,23 +10,23 @@ namespace PainAndGain.Items.Weapons.Melee
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Sanctus");
-            Tooltip.SetDefault("The sword of the very first Paladin\nIt pulses with a strange energy..."); //self explanatory, use n\ for new lines
+            Tooltip.SetDefault("The sword of the very first Crusader\nIt pulses with a radiant energy..."); //self explanatory, use n\ for new lines
         }
         
         public override void SetDefaults()
         {
-            item.damage = 50;                   //(placeholder) base damage
+            item.damage = 45;                   //(placeholder) base damage
             item.melee = true;                  //melee or no
             item.width = 94;                    //hitbox width
             item.height = 92;                   //hitbox height
-            item.useTime = 20;                  //(placeholder) speed weapon is used
-            item.useAnimation = 20;             //(placeholder) speed of animation of weapon
+            item.useTime = 40;                  //(placeholder) speed weapon is used
+            item.useAnimation = 40;             //(placeholder) speed of animation of weapon
             item.useStyle = 1;                  //The use style of weapon, 1 for swinging, 2 for drinking, 3 act like shortsword, 4 for use like life crystal, 5 for use staffs or guns
-            item.knockBack = 6;                 //(placeholder) knockback of weapon. 20 maximum
+            item.knockBack = 9;                 //(placeholder) knockback of weapon. 20 maximum
             item.value = 100000;                // weapon value, might be wrong code
             item.rare = 3;                      //item rarity -1 to 3 = prehardmode, 4 to 11 = hardmode, 12 = expert, 13 = quest
             item.UseSound = SoundID.Item1;      //item use sound. i dont know them all but 1 is definitely a sword
-            item.autoReuse = true;              //autoswing or no
+            item.autoReuse = false;              //autoswing or no
         }
         
         
