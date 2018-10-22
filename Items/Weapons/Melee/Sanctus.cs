5592b78b50cdf107fbf8bc44913cb314e02e1a3a
Yeeto Deleto <44147364+Cataclyzmicc@users.noreply.github.com>
2018-10-22 17:13:31 -0400

    hey catnip i fixed it lol

--- Items/Weapons/Melee/Sanctus.cs
+++ Items/Weapons/Melee/Sanctus.cs
@@ -1,3 +1,4 @@
+using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -8,12 +9,12 @@ namespace PainAndGain.Items.Weapons.Melee
     {
         public override void SetStaticDefaults()
         {
+            DisplayName.SetDefault("Sanctus");
             Tooltip.SetDefault("The sword of the very first Paladin\nIt pulses with a strange energy..."); //self explanatory, use n\ for new lines
         }
         
         public override void SetDefaults()
         {
-            item.name = "Sanctus";              //display name
             item.damage = 50;                   //(placeholder) base damage
             item.melee = true;                  //melee or no
             item.width = 94;                    //hitbox width
@@ -22,11 +23,20 @@ namespace PainAndGain.Items.Weapons.Melee
             item.useAnimation = 20;             //(placeholder) speed of animation of weapon
             item.useStyle = 1;                  //The use style of weapon, 1 for swinging, 2 for drinking, 3 act like shortsword, 4 for use like life crystal, 5 for use staffs or guns
             item.knockBack = 6;                 //(placeholder) knockback of weapon. 20 maximum
-            item.value = 100000;                // weapon, might be wrong code
-            item.rare = 3;                       //item rarity -1 to 3 = prehardmode, 4 to 11 = hardmode, 12 = expert, 13 = quest
+            item.value = 100000;                // weapon value, might be wrong code
+            item.rare = 3;                      //item rarity -1 to 3 = prehardmode, 4 to 11 = hardmode, 12 = expert, 13 = quest
             item.UseSound = SoundID.Item1;      //item use sound. i dont know them all but 1 is definitely a sword
             item.autoReuse = true;              //autoswing or no
         }
         
+        
+        public override void MeleeEffects(Player player, Rectangle hitbox)
+		{
+			if (Main.rand.Next(3) == 0)
+			{
+				//Emit dusts when swing the sword
+				Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, mod.DustType("Sparkle"));  //VERY IMPORTANT TO CHANGE. NOT AN ACTUAL DUST TYPE, BUT WILL BE ADDED IN THE FUTURE.
+			}
+		}
     }
 }
