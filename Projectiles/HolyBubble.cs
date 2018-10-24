9fde7002725f7031c9da0e4629c088c2a7fa5f99
Yeeto Deleto <44147364+Cataclyzmicc@users.noreply.github.com>
2018-10-24 10:04:30 -0400

    updated projectile, try redownloading catnip

--- Projectiles/HolyBubble.cs
+++ Projectiles/HolyBubble.cs
@@ -22,10 +22,10 @@ namespace PainAndGain.Projectiles
 			projectile.aiStyle = 0;             //The ai style of the projectile, please reference the source code of Terraria
 			projectile.friendly = true;         //Can the projectile deal damage to enemies?
 			projectile.hostile = false;         //Can the projectile deal damage to the player?
-			projectile.ranged = true;           //Is the projectile shoot by a ranged weapon?
+			projectile.ranged = false;           //Is the projectile shoot by a ranged weapon?
 			projectile.penetrate = 1;           //How many monsters the projectile can penetrate. (OnTileCollide below also decrements penetrate for bounces as well)
 			projectile.timeLeft = 300;          //The live time for the projectile (60 = 1 second, so 600 is 10 seconds)
-			projectile.alpha = 155;             //The transparency of the projectile, 255 for completely transparent. (aiStyle 1 quickly fades the projectile in)
+			projectile.alpha = 55;             //The transparency of the projectile, 255 for completely transparent. (aiStyle 1 quickly fades the projectile in)
 			projectile.light = 0.75f;            //How much light emit around the projectile
 			projectile.ignoreWater = true;          //Does the projectile's speed be influenced by water?
 			projectile.tileCollide = true;          //Can the projectile collide with tiles?
