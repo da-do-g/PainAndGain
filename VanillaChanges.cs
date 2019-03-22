a97b38fe2ba2d5c43752b25d015610c154b9619d
Yeeto Deleto <44147364+Cataclyzmicc@users.noreply.github.com>
2019-03-22 15:18:35 -0400

    epical

--- VanillaChanges.cs
+++ VanillaChanges.cs
@@ -15,24 +15,16 @@ namespace PainAndGain
 
                     if (Main.hardMode) //if it's hardmode the NPC will sell this
                     {
-                        shop.item[nextSlot].SetDefaults(mod.ItemType("BowName"));  //this is an example of how to add your item
+                        shop.item[nextSlot].SetDefaults(mod.ItemType("Sanctus"));  //the modded item that will be sold
                         nextSlot++;
-                        shop.item[nextSlot].SetDefaults(ItemID.LunarBar);    //this is an example of how to add a terraria item
-                        nextSlot++;
+                        //shop.item[nextSlot].SetDefaults(ItemID.LunarBar);    //this adds vanilla items, won't be used for now
+                        //nextSlot++;
                     }
                     else
                     {    //This make that the npc will always sell this
-                        shop.item[nextSlot].SetDefaults(mod.ItemType("DualUseWeapon"));   //this is an example of how to add your item
-                        nextSlot++;
-                        shop.item[nextSlot].SetDefaults(mod.ItemType("ItemName"));   //this is an example of how to add your item
-                        nextSlot++;
-                        shop.item[nextSlot].SetDefaults(ItemID.IvyWhip);     //this is an example of how to add a terraria item
-                        nextSlot++;
                     }
                     if (Main.player[Main.myPlayer].ZoneJungle)//if the player is in jungle the npc will sell whis.  Change ZoneJungle with what zone you want: ZoneCorrupt for Corupption, ZoneCrimson for Crimson, ZoneDesert for Desert, ZoneDungeon for Dungeon, ZoneGlowshroom for Glowing Mushroom biome, ZoneHoly for The Hallow, ZoneJungle for Jungle, ZoneMeteor for Meteorite biome, ZoneSnow for Snow biome.
                     {
-                        shop.item[nextSlot].SetDefaults(ItemID.Abeemination); //this is an example of how to add a terraria item
-                        nextSlot++;
                     }
 
                     break;
